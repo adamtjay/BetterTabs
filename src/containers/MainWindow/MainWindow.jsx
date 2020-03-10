73d8bea07133b1d85739d4afb65e49a2037d8d0d
Adam Julier <adamjulier@Adams-MBP.lan1>
2020-03-09 20:38:15 -0400

    Close & Create options

--- src/containers/MainWindow/MainWindow.jsx
+++ src/containers/MainWindow/MainWindow.jsx
@@ -1,14 +1,29 @@
+/*global chrome*/
 import React, { Component } from "react";
 import "../../css/App.css";
 import TabsView from "../../components/TabsView/TabsView";
-import SettingsIcon from '@material-ui/icons/Settings';
+import CloseIcon from "@material-ui/icons/Close";
+import SettingsIcon from "@material-ui/icons/Settings";
+import AddIcon from "@material-ui/icons/Add";
 
 export default class MainWindow extends Component {
+   createTab = () => {
+    chrome.tabs.create({}, () => {});
+   }
+
   render() {
     return (
       <div className="main-window">
         <div className="main-header">
-          <SettingsIcon className="settings-icon" fontSize="large" />
+          <AddIcon 
+          className="new-tab-icon"
+          onClick={() => this.createTab()}
+          />
+          <SettingsIcon className="main-settings-icon" />
+          <CloseIcon
+            onClick={() => window.close()}
+            className="main-close-icon"
+          />
           <h1>BetterTabs</h1>
           <div className="main-options">
             <h4 className="main-option-selected">Active</h4>
