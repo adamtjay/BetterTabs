515fdc53b95b9d729c2919b9ef62556039522973
Adam Julier <adamjulier@Adams-MBP.lan1>
2020-04-03 16:12:13 -0400

    Pin, Un-pin tabs feature

--- src/components/TabsView/TabOptions/TabOptions.jsx
+++ src/components/TabsView/TabOptions/TabOptions.jsx
@@ -2,15 +2,10 @@
 import React from "react";
 import Tooltip from "@material-ui/core/Tooltip";
 import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+import RoomOutlinedIcon from "@material-ui/icons/RoomOutlined";
+import RoomIcon from "@material-ui/icons/Room";
 
 export default function TabOptions(props) {
-  function closeTab(id) {
-    chrome.tabs.remove(id, () => {
-      // query X times to account for varying times in closing tabs
-      setIntervalX(() => props.getCurrentTabs(), 100, 5);
-    });
-  }
-
   function setIntervalX(callback, delay, repetitions) {
     let x = 0;
     let intervalID = window.setInterval(function() {
@@ -22,13 +17,43 @@ export default function TabOptions(props) {
     }, delay);
   }
 
+  function closeTab(id) {
+    // Close tab, by id
+    chrome.tabs.remove(id, () => {
+      setIntervalX(() => props.getCurrentTabs(), 100, 5);
+    });
+  }
+
+  function pinUnpinTab(id, isPinned) {
+    // Pin or Un-pin tab, by id & current pinned state
+    if (!isPinned) {
+      chrome.tabs.update(id, { pinned: true }, () => {});
+    } else {
+      chrome.tabs.update(id, { pinned: false }, () => {});
+    }
+    setIntervalX(() => props.getCurrentTabs(), 100, 5);
+  }
+
   return (
     <div className="tab-options">
-      <Tooltip title="Close Tab">
+      <Tooltip title="Close">
         <HighlightOffIcon
           className="icon-close"
           onClick={() => closeTab(props.tab.id)}
         />
+      </Tooltip>
+      <Tooltip title={!props.tab.pinned ? "Pin" : "Un-pin"}>
+        {!props.tab.pinned ? (
+          <RoomOutlinedIcon
+            className="icon-pin"
+            onClick={() => pinUnpinTab(props.tab.id, props.tab.pinned)}
+          />
+        ) : (
+          <RoomIcon
+            className="icon-pin"
+            onClick={() => pinUnpinTab(props.tab.id, props.tab.pinned)}
+          />
+        )}
       </Tooltip>
     </div>
   );
